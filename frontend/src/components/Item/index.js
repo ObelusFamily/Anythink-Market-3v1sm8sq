bacdd6211f43883a8e7df126aabd81529b4a6c77
Jahirul Islam <j.islam.trine@gmail.com>
2022-12-29 21:13:28 +0600

    Fix: Fix for #INC-5352, Missing image.

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -35,7 +35,7 @@ class Item extends React.Component {
     if (!this.props.item) {
       return null;
     }
-
+    let image = this.props.item.image
     const markup = {
       __html: marked(this.props.item.description, { sanitize: true }),
     };
@@ -48,7 +48,7 @@ class Item extends React.Component {
           <div className="row bg-white p-4">
             <div className="col-6">
               <img
-                src={this.props.item.image}
+                src={image ? image : '/placeholder.png'}
                 alt={this.props.item.title}
                 className="item-img"
                 style={{ height: "500px", width: "100%", borderRadius: "6px" }}
